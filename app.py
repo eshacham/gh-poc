5a53d2f6aeb95d5f7fb89e727b4247935afe42b4
Eyal Shacham <shacham@swbell.net>
2025-05-16 07:09:32 -0500

    insecure the code

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import os
 import subprocess
 
-
-def main():
-    print("Hello from GitHub Secure CI/CD POC!")
-
+AWS_SECRET_ACCESS_KEY = 'AKIA1234567890ABCDEF'
 
 def insecure():
     # BAD: reading a shell command from an environment variable
-    #      and passing it straight into subprocess.run with shell=True
+    # and passing it straight into subprocess.run with shell=True
     cmd = os.getenv("BAD_CMD", 'echo "This is a bad command"')
     subprocess.run(cmd, shell=True)
+
+def main():
+    print(f"Hello from GitHub Secure CI/CD POC!: {AWS_SECRET_ACCESS_KEY}")
+    insecure()
 
 
 if __name__ == "__main__":
     main()
-    insecure()
 
 
-AWS_SECRET_ACCESS_KEY = 'AKIA1234567890ABCDEF'
+
