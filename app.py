d7372c55309ed314c71659f4d09f5379b6745ac3
Eyal Shacham <shacham@swbell.net>
2025-05-16 07:25:55 -0500

    fix lint issues

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import subprocess
 
 AWS_SECRET_ACCESS_KEY = 'AKIA1234567890ABCDEF'
 
+
 def insecure():
     # BAD: reading a shell command from an environment variable
     # and passing it straight into subprocess.run with shell=True
     cmd = os.getenv("BAD_CMD", 'echo "This is a bad command"')
     subprocess.run(cmd, shell=True)
+
 
 def main():
     print(f"Hello from GitHub Secure CI/CD POC!: {AWS_SECRET_ACCESS_KEY}")
@@ -16,6 +18,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
 
